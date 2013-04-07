a0353e157ad56403346f7a4ac290ccd84e0badc2
mehdimehdi <mehdi@punchtab.com>
2013-04-06 22:34:04 -0700

    merging admin and index together, and using backbone for the main app UI

--- js/main.js
+++ js/main.js
@@ -30,12 +30,31 @@ $(function() {
 
     //the app view
     var AppView = Parse.View.extend({
-        el:$("#store-locator"),
+        el:$("#app-container"),
         initialize: function() {
             this.render(); 
         },
         render: function() {
-            console.log('app view');
+            this.$el.html(_.template($("#app-template").html()));
+
+
+            $("a").tooltip({
+                'selector': '',
+                'placement': 'bottom'
+            });
+
+            $('.typeahead').typeahead({source:["A.P.C.","Alternative Apparel","Barbour","Burkman Brothers","Commes des Garcons","Creep","Duluth Pack","Eastland","Ebbetts","Etudes","Filson","Fjallraven","Gant","General Assembly","Giles & Brother","Gingko Press","Goorin Bros.","Happy Socks","Jungmaven","Juniper Ridge","Kent Combs","Levi's Made & Crafted ","Life After Denim","Malin + Goetz","Maxx & Unicorn","Miansai","Nanamica","Norse Projects","Otter Wax","Paul Rose","Patch NYC","Pendleton","Pendleton Portland Collection","Penny Skateboards","Poler Camping Stuff","Rag & Bone","Rainbow","Reyn Spooner","Rite in the Rain","Saturdays NYC","Schott NYC","Sebago","Shades of Grey","Shwood","Smathers & Branson","Soap & Paper Factory","Stanley","Tanner Goods","Topman","Uniform Wares","Ursa Major","Vanishing Elephant","Vans", "Velour"]});
+
+            function initialize() {
+                var mapOptions = {
+                        center: new google.maps.LatLng(37.770, -122.440),
+                        zoom: 13,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+            }
+
+            google.maps.event.addDomListener(window, 'load', initialize);
         }
         
     });
